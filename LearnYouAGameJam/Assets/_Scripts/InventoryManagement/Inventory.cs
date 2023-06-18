0d891d72d01cbab123c00cfe23d8650503a64874
Neonalig <codychook@gmail.com>
2023-06-18 16:32:16 +0800

    Added inventory system

---
+++ LearnYouAGameJam/Assets/_Scripts/InventoryManagement/Inventory.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using LYGJ.Common;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace LYGJ.InventoryManagement {
+    public sealed class Inventory : SingletonMB<Inventory>, IReadOnlyCollection<ItemInstance> {
+        readonly Dictionary<Item, uint> _Inventory = new(16);
+
+        [SerializeField, Tooltip("Raised when an item is added to the inventory.")]
+        UnityEvent<ItemInstance> _OnItemAdded = new();
+
+        /// <summary> Raised when an item is added to the inventory. </summary>
+        public static event UnityAction<ItemInstance> Added {
+            add => Instance._OnItemAdded.AddListener(value);
+            remove => Instance._OnItemAdded.RemoveListener(value);
+        }
+
+        [SerializeField, Tooltip("Raised when an item is removed from the inventory.")]
+        UnityEvent<ItemInstance> _OnItemRemoved = new();
+
+        /// <summary> Raised when an item is removed from the inventory. </summary>
+        public static event UnityAction<ItemInstance> Removed {
+            add => Instance._OnItemRemoved.AddListener(value);
+            remove => Instance._OnItemRemoved.RemoveListener(value);
+        }
+
+        [SerializeField, Tooltip("Raised when the inventory is cleared.")]
+        UnityEvent _OnInventoryCleared = new();
+
+        /// <summary> Raised when the inventory is cleared. </summary>
+        public static event UnityAction Cleared {
+            add => Instance._OnInventoryCleared.AddListener(value);
+            remove => Instance._OnInventoryCleared.RemoveListener(value);
+        }
+
+        /// <summary> Adds the specified item to the inventory. </summary>
+        /// <param name="Item"> The item to add. </param>
+        public static void Add( ItemInstance Item ) => Instance.AddInternal(Item);
+
+        /// <inheritdoc cref="Add(ItemInstance)"/>
+        /// <param name="Item"> The item to add. </param>
+        /// <param name="Amount"> The amount of the item to add. </param>
+        public static void Add( Item Item, uint Amount = 1u ) {
+            if (Amount == 0u) {
+                Debug.LogWarning("Cannot add zero items.");
+                return;
+            }
+            Instance.AddInternal(new(Item, Amount));
+        }
+
+        void AddInternal( ItemInstance Item ) {
+            if (_Inventory.ContainsKey(Item.Item)) {
+                _Inventory[Item.Item] += Item.Amount;
+            } else {
+                _Inventory.Add(Item.Item, Item.Amount);
+            }
+            _OnItemAdded.Invoke(Item);
+        }
+
+        /// <summary> Removes the specified item from the inventory. </summary>
+        /// <param name="Item"> The item to remove. </param>
+        /// <exception cref="KeyNotFoundException"> Thrown if the item is not in the inventory. </exception>
+        /// <exception cref="NotEnoughItemsException"> Thrown if there are not enough items to remove. </exception>
+        public static void Remove( ItemInstance Item ) => Instance.RemoveInternal(Item.Item, Item.Amount);
+
+        /// <summary> Removes the specified item from the inventory. </summary>
+        /// <param name="Item"> The item to remove. </param>
+        /// <param name="Amount"> The amount of the item to remove. </param>
+        public static void Remove( Item Item, uint Amount = 1u ) => Instance.RemoveInternal(Item, Amount);
+
+        void RemoveInternal(Item Item, uint Amount) {
+            if (!_Inventory.TryGetValue(Item, out uint CurrentAmount)) {
+                throw new KeyNotFoundException($"Item {Item} is not in the inventory.");
+            }
+            if (CurrentAmount < Amount) {
+                throw new NotEnoughItemsException(Item, Amount, CurrentAmount);
+            }
+            if (CurrentAmount == Amount) {
+                _Inventory.Remove(Item);
+            } else {
+                _Inventory[Item] -= Amount;
+            }
+            _OnItemRemoved.Invoke(new(Item, Amount));
+        }
+
+        /// <summary> Removes all of a specified item from the inventory. </summary>
+        /// <param name="Item"> The item to remove. </param>
+        /// <exception cref="KeyNotFoundException"> Thrown if the item is not in the inventory. </exception>
+        /// <returns> The amount of the item that was removed. </returns>
+        public static uint RemoveAll( Item Item ) => Instance.RemoveAllInternal(Item);
+
+        uint RemoveAllInternal(Item Item) {
+            if (!_Inventory.TryGetValue(Item, out uint Amount)) {
+                throw new KeyNotFoundException($"Item {Item} is not in the inventory.");
+            }
+            _Inventory.Remove(Item);
+            _OnItemRemoved.Invoke(new(Item, Amount));
+            return Amount;
+        }
+
+        /// <summary> Checks if the inventory contains the specified item. </summary>
+        /// <param name="Item"> The item to check for. </param>
+        /// <returns> <see langword="true"/> if the inventory contains the item; otherwise, <see langword="false"/>. </returns>
+        public static bool Contains( Item Item ) => Instance._Inventory.ContainsKey(Item);
+
+        /// <summary> Checks if the inventory contains the specified item in the given amount. </summary>
+        /// <param name="Item"> The item to check for. </param>
+        /// <returns> <see langword="true"/> if the inventory contains an equivalent or greater amount of the item; otherwise, <see langword="false"/>. </returns>
+        public static bool Contains( ItemInstance Item ) => Instance._Inventory.TryGetValue(Item.Item, out uint Amount) && Amount >= Item.Amount;
+
+        /// <inheritdoc cref="Contains(Item)"/>
+        /// <param name="Item"> The item to check for. </param>
+        /// <param name="Amount"> The amount of the item to check for. </param>
+        public static bool Contains( Item Item, uint Amount ) {
+            if (Amount == 0u) {
+                Debug.LogWarning("Should not check for zero items.");
+                return true;
+            }
+            return Instance._Inventory.TryGetValue(Item, out uint CurrentAmount) && CurrentAmount >= Amount;
+        }
+
+        /// <summary> Gets the amount of the specified item in the inventory. </summary>
+        /// <param name="Item"> The item to get the amount of. </param>
+        /// <returns> The amount of the item in the inventory, or <c>0</c> if the item is not in the inventory. </returns>
+        public static uint Count( Item Item ) => Instance._Inventory.TryGetValue(Item, out uint Amount) ? Amount : 0u;
+
+        /// <summary> Gets the amount of unique items in the inventory. </summary>
+        /// <returns> The amount of unique items in the inventory. </returns>
+        public static int UniqueItemCount => Instance._Inventory.Count;
+
+        #region Implementation of IEnumerable
+
+        /// <inheritdoc />
+        public IEnumerator<ItemInstance> GetEnumerator() {
+            foreach (KeyValuePair<Item, uint> Pair in _Inventory) {
+                yield return Pair;
+            }
+        }
+
+        /// <inheritdoc cref="GetEnumerator"/>
+        public static IEnumerator<ItemInstance> All => Instance.GetEnumerator();
+
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        #endregion
+
+        #region Implementation of IReadOnlyCollection<out ItemInstance>
+
+        /// <inheritdoc />
+        int IReadOnlyCollection<ItemInstance>.Count => _Inventory.Count;
+
+        #endregion
+
+    }
+
+    public abstract class InventoryException : Exception {
+        protected InventoryException( string Message ) : base(Message) { }
+    }
+
+    public sealed class NotEnoughItemsException : InventoryException {
+        public NotEnoughItemsException( Item Item, uint Requested, uint Available ) : base($"Not enough items. Requested {Requested:N0} {Item}, but only {Available:N0} {Item} are available.") { }
+    }
+}
---
+++ LearnYouAGameJam/Assets/_Scripts/InventoryManagement/Item.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using LYGJ.Common;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace LYGJ.InventoryManagement {
+    public sealed class Item : ScriptableObject, IEquatable<Item>, IComparable<Item>, IComparable {
+        /// <summary> The item ID. </summary>
+        [field: SerializeField, Tooltip("The item ID.")] public string ID { get; private set; } = string.Empty;
+
+        /// <summary> The item name. </summary>
+        [field: SerializeField, Tooltip("The item name.")] public string Name { get; private set; } = string.Empty;
+
+        /// <summary> The item description. </summary>
+        [field: SerializeField, Multiline, Tooltip("The item description.")] public string Description { get; private set; } = string.Empty;
+
+        #if UNITY_EDITOR
+        void Reset() {
+            string Path = UnityEditor.AssetDatabase.GetAssetPath(this);
+            string Name = System.IO.Path.GetFileNameWithoutExtension(Path);
+            ID        = Name;
+            this.Name = Name.ConvertNamingConvention(NamingConvention.TitleCase);
+        }
+        #endif
+
+        #region Overrides of Object
+
+        /// <inheritdoc />
+        public override string ToString() => $"{Name} ({ID})";
+
+        #endregion
+
+        #region Equality Members
+
+        /// <inheritdoc />
+        public bool Equals( Item? Other ) {
+            if (ReferenceEquals(null, Other)) {
+                return false;
+            }
+
+            if (ReferenceEquals(this, Other)) {
+                return true;
+            }
+
+            return base.Equals(Other)
+                && ID == Other.ID;
+        }
+
+        /// <inheritdoc />
+        public override bool Equals( object? Obj ) => ReferenceEquals(this, Obj) || Obj is Item Other && Equals(Other);
+
+        /// <inheritdoc />
+        public override int GetHashCode() => HashCode.Combine(base.GetHashCode(), ID);
+
+        public static bool operator ==( Item? Left, Item? Right ) => Equals(Left, Right);
+        public static bool operator !=( Item? Left, Item? Right ) => !Equals(Left, Right);
+
+        #endregion
+
+        #region Relational Members
+
+        /// <inheritdoc />
+        public int CompareTo( Item? Other ) {
+            if (ReferenceEquals(this, Other)) {
+                return 0;
+            }
+
+            if (ReferenceEquals(null, Other)) {
+                return 1;
+            }
+
+            return string.Compare(Name, Other.Name, StringComparison.Ordinal);
+        }
+
+        /// <inheritdoc />
+        public int CompareTo( object? Obj ) {
+            if (ReferenceEquals(null, Obj)) {
+                return 1;
+            }
+
+            if (ReferenceEquals(this, Obj)) {
+                return 0;
+            }
+
+            return Obj is Item Other ? CompareTo(Other) : throw new ArgumentException($"Object must be of type {nameof(Item)}");
+        }
+
+        public static bool operator <( Item?  Left, Item? Right ) => Comparer<Item?>.Default.Compare(Left, Right) < 0;
+        public static bool operator >( Item?  Left, Item? Right ) => Comparer<Item?>.Default.Compare(Left, Right) > 0;
+        public static bool operator <=( Item? Left, Item? Right ) => Comparer<Item?>.Default.Compare(Left, Right) <= 0;
+        public static bool operator >=( Item? Left, Item? Right ) => Comparer<Item?>.Default.Compare(Left, Right) >= 0;
+
+        #endregion
+
+        public static ItemInstance operator *( Item Item, in uint Amount ) => new(Item, Amount);
+
+    }
+
+    [Serializable]
+    public readonly struct ItemInstance {
+        /// <summary> The item. </summary>
+        [ShowInInspector, HorizontalGroup]
+        public readonly Item Item;
+
+        /// <summary> The item amount. </summary>
+        [ShowInInspector, HorizontalGroup, LabelText("x"), LabelWidth(20f)]
+        public readonly uint Amount;
+
+        /// <summary> The item name. </summary>
+        /// <param name="Item"> The item. </param>
+        /// <param name="Amount"> The item amount. </param>
+        /// <exception cref="ArgumentNullException"> Thrown if <paramref name="Item"/> is null. </exception>
+        /// <exception cref="ArgumentOutOfRangeException"> Thrown if <paramref name="Amount"/> is zero. </exception>
+        public ItemInstance( Item Item, in uint Amount = 1u ) {
+            if (Item   == null) { throw new ArgumentNullException(nameof(Item), "Item cannot be null."); }
+            if (Amount == 0u) { throw new ArgumentOutOfRangeException(nameof(Amount), Amount, "Amount must be greater than zero."); }
+
+            this.Item   = Item;
+            this.Amount = Amount;
+        }
+
+        #region Casts
+
+        public static implicit operator Item( in ItemInstance ItemInstance ) => ItemInstance.Item;
+        public static implicit operator uint( in ItemInstance ItemInstance ) => ItemInstance.Amount;
+
+        public static implicit operator ItemInstance( in Item Item ) => new(Item, 1u);
+
+        public static implicit operator ItemInstance( KeyValuePair<Item, uint> Pair ) => new(Pair.Key, Pair.Value);
+
+        #endregion
+
+        #region Operators
+
+        public static ItemInstance operator +( in ItemInstance Left, in uint Right ) => new(Left.Item, Left.Amount + Right);
+        public static ItemInstance operator +( in ItemInstance Left, in ItemInstance Right ) {
+            if (Left.Item != Right.Item) { throw new InvalidOperationException("Cannot add two different items."); }
+            return new(Left.Item, Left.Amount + Right.Amount);
+        }
+
+        public static ItemInstance operator -( in ItemInstance Left, in uint Right ) => new(Left.Item, Left.Amount - Right);
+        public static ItemInstance operator -( in ItemInstance Left, in ItemInstance Right ) {
+            if (Left.Item   != Right.Item) { throw new InvalidOperationException("Cannot subtract two different items."); }
+            if (Left.Amount < Right.Amount) { throw new InvalidOperationException("Cannot subtract more items than exist."); }
+            if (Left.Amount == Right.Amount) { throw new InvalidOperationException("Cannot subtract all items."); }
+
+            return new(Left.Item, Left.Amount - Right.Amount);
+        }
+
+        #endregion
+
+        /// <summary> Deconstructs the item instance. </summary>
+        /// <param name="Item"> The item. </param>
+        /// <param name="Amount"> The item amount. </param>
+        public void Deconstruct( out Item Item, out uint Amount ) {
+            Item   = this.Item;
+            Amount = this.Amount;
+        }
+
+        #region Overrides of ValueType
+
+        /// <inheritdoc />
+        public override string ToString() => $"{Item} x{Amount:N0}";
+
+        #endregion
+
+    }
+}
